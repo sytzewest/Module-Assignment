159428924d6f10dfe42b37f1015bc71d6678d570
sytzewest <43606602+sytzewest@users.noreply.github.com>
2018-11-06 14:09:28 +0100

    Fixed the contact page php

--- contact.php
+++ contact.php
@@ -1,25 +1,51 @@
+<!DOCTYPE html>
+
 <?php
- if (isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
 
-                    if (
-                        !empty($_POST['name']) &&
-                        !empty($_POST['subject']) &&
-                        !empty($_POST['message']) &&
-                        !empty($_POST['email'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $email = $_POST['email'];
 
-                        $name = $_POST['name'];
-                        $email = $_POST['email'];
-                        $subject = $_POST['subject'];
-                        $message = $_POST['message'];
-                       
+    if (!empty($name)) {
+        echo "<br>Name: " . $name;
+    } else {
+        echo "The name field is empty, please fill it in!";
+    }
 
-                    
-                        echo "Thanks for your message. We will contact you soon.";
-                        
-                        
-                     else {
-                        echo "please fill in all the boxes";
-                    }
-                }
+    if (!empty($subject)) {
+        echo "<br>Subject: " . $subject;
+    } else {
+        echo "<br>The subject field is empty, please fill it in!";
+    }
+
+    if (!empty($message)) {
+        echo "<br>Message: " . $message;
+    } else {
+        echo "<br>The message field is empty, please fill it in!";
+    }
+
+    if (!empty($email)) {
+        echo "<br>Email: " . $email;
+    } else {
+        echo "<br>The email field is empty, please fill it in!";
+    }
+
+    if (!empty($email) && (!empty($subject)) && (!empty($message)) && (!empty($email))) {
+        echo "<br>Thank you, the details above been submitted, we will get back to within 72 hours.";
+    } else {
+        echo "Please fill in all the required fields!";
+    }
+}
 ?>
 
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="style.css" type="text/css" rel="stylesheet">
+        <title></title>
+    </head>
+    <body>
+    </body>
+</html>
